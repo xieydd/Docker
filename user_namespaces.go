96c04d3831844210093e848efcc428d8922292c8
xieydd <xieydd@gmail.com>
2018-06-12 22:15:30 +0800

    这里的user_namespaces有问题，3.10内核的问题，还未处理，已提issue:https://github.com/xianlubird/mydocker/issues/3,持续关注，目前在4.4上可以使用

--- user_namespaces.go
+++ user_namespaces.go
@@ -7,6 +7,12 @@ import (
 	"syscall"
 )
 
+// 我们发现在宿主机环境下为root 1 ，在namespace环境下为1234
+// [root@00-0c-29-4a-bf-33 Docker]# id
+// uid=0(root) gid=0(root) groups=0(root)
+// [root@00-0c-29-4a-bf-33 Docker]# go run user_namespaces.go
+// sh-4.2$ id
+// uid=1234 gid=1234 groups=1234
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
