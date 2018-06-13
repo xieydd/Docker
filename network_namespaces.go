6507377d945e370a7e741958060be7477a22049b
xieydd <xieydd@gmail.com>
2018-06-13 15:50:36 +0800

    对namespaces隔离网络测试

---
+++ network_namespaces.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// 我们发现隔离后使用ifconfig没有网络设备，所以和宿主机是隔离的
+// sh-4.2$ ifconfig
+// sh-4.2$
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+	}
+
+	//cmd.SysProcAttr.Credential = &syscall.Credential{
+	//	Uid: uint32(1),
+	//	Gid: uint32(1),
+	//}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(-1)
+}
